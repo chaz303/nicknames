854d3940fb9986eae940ff43183acdc3e0d51224
Chaz <charlesssumnerwilson@gmail.com>
2019-01-27 21:02:31 +0900

    working seeder

--- migrations/migration_file.js
+++ migrations/migration_file.js
@@ -1,32 +1,31 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-      knex.schema.createTable("name", (table) => {
-          table.increments("id").primary();
-          table.string("name").unique();
-          table.string("name_normed").index();
-          table.boolean("is_masculine").defaultTo(0);
-          table.boolean("is_feminine").defaultTo(0);
-      }),
-      knex.schema.createTable("nickname", (table) => {
-          table.increments("id").primary();
-          table.string("nickname").unique();
-          table.string("nickname_normed").index();
-          table.boolean("is_masculine").defaultTo(0);
-          table.boolean("is_feminine").defaultTo(0);
-      }),
-      knex.schema.createTable("name_to_nickname", (table) => {
-          table.increments("id").primary();
-          table.integer("name_id");
-          table.integer("nickname_id");
-      }),
+    knex.schema.createTable("name", (table) => {
+      table.integer("id").primary();
+      table.string("name").unique();
+      table.string("name_normed").index();
+      table.boolean("is_masculine").defaultTo(0);
+      table.boolean("is_feminine").defaultTo(0);
+    }),
+    knex.schema.createTable("nickname", (table) => {
+      table.integer("id").primary();
+      table.string("nickname").unique();
+      table.string("nickname_normed").index();
+      table.boolean("is_masculine").defaultTo(0);
+      table.boolean("is_feminine").defaultTo(0);
+    }),
+    knex.schema.createTable("name_to_nickname", (table) => {
+      table.increments("id").primary();
+      table.integer("name_id");
+      table.integer("nickname_id");
+    }),
   ]).catch((err) => console.log(err));
 };
-  
+
 exports.down = function(knex, Promise) {
   return Promise.all([
-      knex.schema.dropTable("name"),
-      knex.schema.dropTable("nickname"),
-      knex.schema.dropTable("name_to_nickname")
-  ])    
+    knex.schema.dropTable("name"),
+    knex.schema.dropTable("nickname"),
+    knex.schema.dropTable("name_to_nickname"),
+  ]);
 };
-  
--- seeds/seed_name.js
+++ seeds/seed_name.js
@@ -1,76 +1,25 @@
-//fs = require('fs');
-const filesomething = require("./seeddata.csv")
-const seedNickname = "Pancho";
-const seedName = "Francisco";
-const seedGender = "m";
+console.log(process.cwd());
+
+const nameData = require("./names.json");
+const nicknameData = require("./nicknames.json");
+const nameToNicknameData = require("./names_to_nicknames.json");
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  console.log("something-->",filesomething);
-  return knex('nickname').del()
-    .then(function () {
-      // Inserts seed entries
-      if (seedGender === "m"){  
-        return knex('nickname').insert([
-        {nickname: seedNickname,
-        nickname_normed: seedNickname
-        .normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase(), 
-        is_masculine: 1
-        }
-      ]);
-    } else {
-        return knex('nickname').insert([
-        {nickname: seedNickname,
-        nickname_normed: seedNickname
-        .normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase(),
-        is_feminine: 1
-        }
-      ]);
-    }
-  }).then(function () {
-    return
-  }
-
-  )
-};
-
-
-
-
-
-
-function readSeedData() {
-    fs.readFile('./migrations/seeddata.csv', 'utf8', (err, data) => {
-        if (err) {
-            return console.log(process.cwd(), err);
-        }
-        return data;
+  return knex("nickname")
+    .del()
+    .then(function() {
+      return knex("name").del();
     })
-}
-
-function insertSeedData (knex, seedNickname, seedName, seedGender) {
-    const seedNicknameNormed = seedNickname.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
-    const seedNameNormed = seedName.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
-    // this code should check to see if a given nickname is already in the database
-    //const nicknameId = knex.select(id).from('nickname').where(nickname = seedNickname);
-    if (true /*!nicknameId*/){
-        if (seedGender === "m"){
-            knex('nickname').insert({nickname: seedNickname},
-            {nickname_normed: seedNicknameNormed}, {is_masculine: 1});
-        } else {
-            knex('nickname').insert({nickname: seedNickname},
-            {nickname_normed: seedNicknameNormed}, {is_feminine: 1});
-        }
-        //nicknameId = knex.select(id).from('nickname').where(nickname = seedNickname);
-    }
-    //const nameId = knex.select(id).from('name').where(name = seedName);
-    if (true /*!nameId*/){
-        if (seedGender === "m"){
-            knex('name').insert({name: seedName}, {name_normed: seedNameNormed}, {is_masculine: 1}).catch((err) => {console.log("error", err)});
-        } else {
-            knex('name').insert({name: seedName}, {name_normed: seedNameNormed}, {is_feminine: 1});
-        }
-        //nameId = knex.select(id).from('name').where(name = seedName);
-    }
-    //knex('name_to_nickname').insert({nickname_id: nicknameId}, {name_id: nameId});
-}
+    .then(function() {
+      return knex("name_to_nickname").del();
+    })
+    .then(function() {
+      return knex("nickname").insert(nicknameData);
+    })
+    .then(function() {
+      return knex("name").insert(nameData);
+    })
+    .then(function() {
+      return knex("name_to_nickname").insert(nameToNicknameData);
+    });
+};
